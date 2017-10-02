e8ba8ec68ec70e168a4cba230c2e68b7be478fe8
Joe Smith <yasumoto7@gmail.com>
2017-10-02 12:28:44 -0700

    Correct version of CryptoSwift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "Gyutou", targets: ["Gyutou"])
     ],
     dependencies: [
-        .package(url: "https://github.com/berenjena-power/CryptoSwift", .branch("swift4"))
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "0.7.2")
     ],
     targets: [
         .target(
